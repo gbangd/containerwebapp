8949c920913ec442ccf01c4a35e7247c76c7c882
gbangd <gbanguera@gmail.com>
2025-06-13 15:22:57 +0000

    Change completed

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace containerwebapp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "AnotherSummary"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
